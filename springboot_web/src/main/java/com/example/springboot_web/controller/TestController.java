63de08e76a593dc344d6f38ffcdb3ccb698a93b6
1193131227@qq.com <1193131227@qq.com>
2022-10-04 14:52:45 +0800

    提交

---
+++ springboot_web/src/main/java/com/example/springboot_web/controller/TestController.java
@@ -0,0 +1,22 @@
+package com.example.springboot_web.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * ClassName TestController
+ * Description TODO
+ * Author 11931
+ * Date 2022-10-04:10:57
+ * Version 1.0
+ **/
+@RestController
+public class  TestController {
+
+    @RequestMapping("/")
+    public String hello(){
+        return "aaaaa";
+    }
+
+
+}
