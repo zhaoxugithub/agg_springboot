8b67ccb4b79038f7e898c7d90639fd4cc0363408
serenipity <1193131227@qq.com>
2022-10-05 16:04:00 +0800

    config

--- springboot_web/src/main/java/com/example/springboot_web/controller/TestController.java
+++ springboot_web/src/main/java/com/example/springboot_web/controller/TestController.java
@@ -1,7 +1,6 @@
 package com.example.springboot_web.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * ClassName TestController
@@ -11,12 +10,38 @@ import org.springframework.web.bind.annotation.RestController;
  * Version 1.0
  **/
 @RestController
-public class  TestController {
+public class TestController {
 
     @RequestMapping("/hello")
-    public String hello(){
+    public String hello() {
         return "aaaaa";
     }
 
 
+    //    @RequestMapping(value = "/user",method = RequestMethod.GET)
+    @GetMapping("/user")
+    public String getUser() {
+
+        return "GET-张三";
+    }
+
+    //    @RequestMapping(value = "/user",method = RequestMethod.POST)
+    @PostMapping("/user")
+    public String saveUser() {
+        return "POST-张三";
+    }
+
+    //    @RequestMapping(value = "/user",method = RequestMethod.PUT)
+    @PutMapping("/user")
+    public String putUser() {
+
+        return "PUT-张三";
+    }
+
+    @DeleteMapping("/user")
+//    @RequestMapping(value = "/user",method = RequestMethod.DELETE)
+    public String deleteUser() {
+        return "DELETE-张三";
+    }
+    // 扩展点：如何把 _method 这个名字换成我们自己喜欢的
 }
